7416c90963e191de14fc81afabad56059b1b6324
Sageeth-Mandil <sageethmandil.me@gmail.com>
2024-04-06 16:46:02 +0530

    reconvert to beginning

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div class="container"></div>
+  )
+}
+
+export default App
--- src/renderer/src/utils/standard-to-metric.ts
+++
@@ -1,3 +0,0 @@
-const convertTemp = (temp: number) => Math.round(temp - 273.15);
-
-export default { convertTemp };
